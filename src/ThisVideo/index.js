f74246a8da5be22851e0e99dfeb43161db7edbfb
tylermcrobert <tyler.mcrobert@gmail.com>
2019-07-31 15:11:21 -0700

    Icons file

--- src/ThisVideo/index.js
+++ src/ThisVideo/index.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react'
 // import PropTypes from 'prop-types'
 import VideoProvider, { VideoCtx, SeekBar } from '../VideoProvider'
 import Styled from './Styled'
+import { FullScreen, Play } from './Icons'
 
 function PrettyVideo({ src, muted, autoPlay, loop }) {
   return (
@@ -45,28 +46,6 @@ function VideoContent() {
     </Styled.Wrapper>
   )
 }
-
-const FullScreen = () => (
-  <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg">
-    <g stroke="#FFF" stroke-width="1.5" fill="none">
-      <path d="M1 8V1h7M13 6v7H6" />
-    </g>
-  </svg>
-)
-
-const Play = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    version="1.1"
-    x="0"
-    y="0"
-    viewBox="0 0 17.2 20.6"
-    width="14"
-    fill="white"
-  >
-    <polygon points="0 0 0 20.6 17.2 10.3 " />
-  </svg>
-)
 
 // PrettyVideo.propTypes = {}
 
