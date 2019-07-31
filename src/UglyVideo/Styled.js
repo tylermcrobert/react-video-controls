91171e4ca841bb37d5e6d2ac1e7c0f55e320f6a3
tylermcrobert <tyler.mcrobert@gmail.com>
2019-07-31 10:58:33 -0700

    Simplify uglyVideo

--- src/UglyVideo/Styled.js
+++ src/UglyVideo/Styled.js
@@ -1,35 +1,14 @@
-import styled, { css } from 'styled-components/macro'
-import * as Controls from '../VideoProvider'
+import styled from 'styled-components/macro'
 
-const Wrapper = styled.div``
-
-const buttonStyle = css`
-  display: inline-block;
-  padding: 0.25rem;
-  appearance: button;
-  background: #eee;
-  border: 1px solid #ccc;
-  margin-right: 0.2rem;
+const Wrapper = styled.div`
+  button {
+    display: inline-block;
+    padding: 0.25rem;
+    appearance: button;
+    background: #eee;
+    border: 1px solid #ccc;
+    margin-right: 0.2rem;
+  }
 `
 
-const Play = styled(Controls.Play)`
-  ${buttonStyle}
-`
-
-const Pause = styled(Controls.Pause)`
-  ${buttonStyle}
-`
-
-const Mute = styled(Controls.Mute)`
-  ${buttonStyle}
-`
-
-const Unmute = styled(Controls.Unmute)`
-  ${buttonStyle}
-`
-
-const Fullscreen = styled(Controls.Fullscreen)`
-  ${buttonStyle}
-`
-
-export default { Play, Pause, Wrapper, Mute, Unmute, Fullscreen }
+export default { Wrapper }
--- src/UglyVideo/index.js
+++ src/UglyVideo/index.js
@@ -2,33 +2,26 @@ import React, { useContext } from 'react'
 // import PropTypes from 'prop-types'
 import VideoProvider, { VideoCtx, SeekBar } from '../VideoProvider'
 import Styled from './Styled'
+import { Mute, Play, Pause, Unmute, Fullscreen } from '../VideoProvider'
 
-function PrettyVideo({ src, muted, autoPlay, loop }) {
-  return (
-    <VideoProvider {...{ src, muted, autoPlay, loop }}>
-      <VideoContent />
-    </VideoProvider>
-  )
-}
-
-function VideoContent() {
+function UglyVideo() {
   const { video, state } = useContext(VideoCtx)
   return (
-    <div>
+    <Styled.Wrapper>
       {video}
-      <Styled.Wrapper>
-        <SeekBar />
-        <Styled.Pause>Pause</Styled.Pause>
-        <Styled.Play>Play</Styled.Play>
-        {state.formatted.time} / {state.formatted.duration}
-        <Styled.Unmute>Un-Mute</Styled.Unmute>
-        <Styled.Mute>Mute</Styled.Mute>
-        <Styled.Fullscreen>FullScreen</Styled.Fullscreen>
-      </Styled.Wrapper>
-    </div>
+      <SeekBar />
+      <Play>Play</Play>
+      <Pause>Pause</Pause>
+      {state.formatted.time} / {state.formatted.duration}
+      <Mute>Mute</Mute>
+      <Unmute>Un-Mute</Unmute>
+      <Fullscreen>FullScreen</Fullscreen>
+    </Styled.Wrapper>
   )
 }
 
-// PrettyVideo.propTypes = {}
-
-export default PrettyVideo
+export default ({ src, muted, autoPlay, loop }) => (
+  <VideoProvider {...{ src, muted, autoPlay, loop }}>
+    <UglyVideo />
+  </VideoProvider>
+)
