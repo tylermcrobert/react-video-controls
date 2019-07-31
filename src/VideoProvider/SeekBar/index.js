c4ad47902f20240e125320da50de2b44090d4ce7
tylermcrobert <tyler.mcrobert@gmail.com>
2019-07-31 12:33:49 -0700

    Man idk

--- src/VideoProvider/SeekBar/index.js
+++ src/VideoProvider/SeekBar/index.js
@@ -1,21 +1,69 @@
-import React from 'react'
+import React, {
+  useState,
+  useRef,
+  createContext,
+  useContext,
+  useEffect,
+  createRef,
+} from 'react'
 import Styled from './Styled'
-import useSeek from './hooks/useSeek'
+
+function useControls() {
+  const parentRef = createRef()
+  const childRef = createRef()
+
+  const percent = 0.7
+
+  useEffect(
+    () => {
+      let dragging = false
+      const $child = childRef.current
+      const $parent = parentRef.current
+
+      function handlemouseDown() {
+        dragging = false
+      }
+
+      function handleMouseMove(e) {
+        const { offsetX } = e
+
+        console.log(offsetX, $parent.offsetLeft)
+
+        $child.style.transform = `translateX(${offsetX}px)`
+      }
+
+      $parent.addEventListener('mousedown', handlemouseDown)
+      window.addEventListener('mousemove', handleMouseMove)
+
+      return () => {
+        $parent.removeEventListener('mousedown', handlemouseDown)
+      }
+    },
+    [childRef, parentRef]
+  )
+
+  return { percent, parentRef, childRef }
+}
+
+const SeekBarCtx = createContext()
 
 function SeekBar() {
-  const { handleChange, value, handleMouseUp, max } = useSeek()
+  const { percent, childRef, parentRef } = useControls()
   return (
-    <Styled.Range
-      type="range"
-      max={max}
-      min="0"
-      onChange={handleChange}
-      value={value}
-      onMouseUp={handleMouseUp}
-    />
+    <Styled.Bar ref={parentRef}>
+      <SeekBarCtx.Provider value={{ percent, childRef }}>
+        <Progress />
+      </SeekBarCtx.Provider>
+    </Styled.Bar>
   )
+}
+
+function Progress() {
+  const { percent, childRef } = useContext(SeekBarCtx)
+  return <Styled.Progress percent={percent} ref={childRef} />
 }
 
 // SeekBar.propTypes = {}
 
+SeekBar.Progress = Progress
 export default SeekBar
