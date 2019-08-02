7a58086183d63f173d8dd47764f663ff13571fb7
tylermcrobert <tyler.mcrobert@gmail.com>
2019-08-02 13:13:11 -0700

    Set up for library

--- src/VideoProvider/util/formatSecs.js
+++
@@ -1,7 +0,0 @@
-import TimeFormat from 'hh-mm-ss'
-
-export default function formatSecs(secs) {
-  const rounded = Math.round(secs)
-  const formatted = TimeFormat.fromS(rounded)
-  return formatted
-}
