9ce0d63402e0787fa6c2aed72d2c9504254038c5
Tyler <tyler.mcrobert@gmail.com>
2019-08-02 18:21:08 -0700

    Included original files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { createContext, useRef } from 'react'
+import PropTypes from 'prop-types'
+import { useVideo } from 'react-use'
+import Styled from './Styled'
+import SeekBar from './SeekBar/index'
+import { Play, Pause, Mute, Unmute, Fullscreen } from './Buttons/index'
+import useCtxSupliment from './hooks/useCtxSupliment'
 
-export default function Test({ children }) {
-  return (
-    <div
-      style={{ padding: 5, border: '1px solid black', display: 'inline-block' }}
-    >
-      Component is working. Input: <strong>{children}</strong>
-    </div>
-  )
+export const VideoCtx = createContext()
+
+function VideoProvider({ children, ...videoProps }) {
+  const wrapperRef = useRef()
+  const playerData = useVideo(<Styled.Video {...videoProps} />)
+  const ctxVal = useCtxSupliment(playerData, wrapperRef)
+  return <VideoCtx.Provider value={ctxVal}>{children}</VideoCtx.Provider>
 }
+
+VideoProvider.propTypes = {
+  src: PropTypes.string.isRequired,
+}
+
+export { Play, Pause, SeekBar, Mute, Unmute, Fullscreen }
+export default VideoProvider
