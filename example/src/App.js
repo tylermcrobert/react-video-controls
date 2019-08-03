dc736b83cbe84f550d62fa087fac2749e06936d2
Tyler <tyler.mcrobert@gmail.com>
2019-08-02 18:03:06 -0700

    Working example

---
+++ example/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Test from 'react-video-controls'
+
+function App() {
+  return (
+    <div className="App">
+      <Test>I'm an example</Test>
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
 export default function Test({ children }) {
-  return <div>test: {children}</div>
+  return (
+    <div
+      style={{ padding: 5, border: '1px solid black', display: 'inline-block' }}
+    >
+      Component is working. Input: <strong>{children}</strong>
+    </div>
+  )
 }
